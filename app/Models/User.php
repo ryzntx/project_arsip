a4d35953534f4eda38c858e3beb8598579be2fdb
ryzntx <thalalatha13@gmail.com>
2024-10-04 09:47:09 +0700

    Perbaikan Layout & Perbaikan Edit Profile

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Facades\DB;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use HasFactory, Notifiable;
 
     protected $table = 'users';
@@ -25,7 +24,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role'
+        'role',
+        'photo_path',
     ];
 
     /**
@@ -43,24 +43,20 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
 
-    public function allData()
-    {
+    public function allData() {
         return DB::table('users')->get();
     }
-    public function addData($data)
-    {
+    public function addData($data) {
         DB::table('users')->insert($data);
     }
-    public function deleteData($data)
-    {
+    public function deleteData($data) {
         DB::table('users')->where('id', $data)->delete();
     }
 }
