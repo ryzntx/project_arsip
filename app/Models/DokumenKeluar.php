de8443537ae2b0f77ed86e0b67f970675eaad2a6
ryzntx <thalalatha13@gmail.com>
2024-10-08 11:12:41 +0700

    chore: Update models DokumenKeluar and DokumenMasuk with fillable fields and relationships

--- app/Models/DokumenKeluar.php
+++ app/Models/DokumenKeluar.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class DokumenKeluar extends Model
-{
+class DokumenKeluar extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'no_dokumen',
+        'nama_dokumen',
+        'penerima',
+        'pengirim',
+        'lampiran',
+        'tanggal_keluar',
+        'status',
+        'persetujuan',
+        'keterangan',
+        'instansi_id',
+        'dokumen_kategori_id',
+        'user_id',
+    ];
+
+    public function instansi(): BelongsTo {
+        return $this->belongsTo(Instansi::class);
+    }
+
+    public function dokumen_kategori(): BelongsTo {
+        return $this->belongsTo(DokumenKategori::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/DokumenMasuk.php
+++ app/Models/DokumenMasuk.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class DokumenMasuk extends Model
-{
+class DokumenMasuk extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'no_dokumen',
+        'nama_dokumen',
+        'penerima',
+        'pengirim',
+        'lampiran',
+        'tanggal_masuk',
+        'keterangan',
+        'instansi_id',
+        'dokumen_kategori_id',
+        'user_id',
+    ];
+
+    public function instansi(): BelongsTo {
+        return $this->belongsTo(Instansi::class);
+    }
+
+    public function dokumen_kategori(): BelongsTo {
+        return $this->belongsTo(DokumenKategori::class);
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
