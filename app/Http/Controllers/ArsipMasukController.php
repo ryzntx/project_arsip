643274b1ac7da3d1027b8457221d005859d43676
ryzntx <thalalatha13@gmail.com>
2024-12-24 18:35:16 +0700

    fix: enhance file upload validation and improve document processing in ArsipMasukController

--- app/Http/Controllers/ArsipMasukController.php
+++ app/Http/Controllers/ArsipMasukController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\DokumenKategori;
-use App\Models\DokumenMasuk;
+use DateTime;
+use DateTimeZone;
+use App\PdfOptimzer;
 use App\Models\Instansi;
+use App\OfficeConverter;
+use App\Models\DokumenMasuk;
 use Illuminate\Http\Request;
+use App\Models\DokumenKategori;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
 
 class ArsipMasukController extends Controller
@@ -37,17 +41,97 @@ class ArsipMasukController extends Controller
     {
         $arsip_masuk = DokumenMasuk::findOrFail($id);
 
+        $validate = $request->validate([
+            'nama_dokumen' => 'required',
+            'nama_penerima' => 'required',
+            'nama_pengirim' => 'required',
+            'tanggal_masuk' => 'required',
+            'dinas_id' => 'required',
+            'kategori_id' => 'required',
+        ], [
+            'nama_dokumen.required' => 'Nama dokumen wajib diisi!',
+            'nama_penerima.required' => 'Penerima wajib diisi!',
+            'nama_pengirim.required' => 'Pengirim wajib diisi!',
+            'tanggal_masuk.required' => 'Tanggal wajib diisi!',
+            'dinas_id.required' => 'Dinas/Instansi wajib diisi!',
+            'kategori_id.required' => 'Kategori dokumen wajib diisi!',
+        ]);
+
         $data = [
             'nama_dokumen' => $request->nama_dokumen,
             'penerima' => $request->nama_penerima,
             'pengirim' => $request->nama_pengirim,
-            'tanggal_masuk' => $request->tanggal_keluar,
+            'tanggal_masuk' => $request->tanggal_masuk,
             'keterangan' => $request->keterangan,
             'instansi_id' => $request->dinas_id,
             'dokumen_kategori_id' => $request->kategori_id,
             'user_id' => auth()->user()->id,
-
         ];
+
+        if (
+            $request->hasFile("file_dokumen") &&
+            $request->file("file_dokumen")->isValid()
+        ) {
+
+            $validate = $request->validate([
+                'file_dokumen' => 'required|mimes:doc,docx,pdf',
+            ], [
+                'file_dokumen.required' => 'Lampiran wajib diisi!',
+                'file_dokumen.mimes' => 'File tidak valid. Hanya mendukung format doc | docx | pdf',
+            ]);
+
+            $dateTime = new DateTime("now", new DateTimeZone("Asia/Jakarta"));
+            $dtFormat = $dateTime->format("dmY_His");
+
+            $file = $request->file("file_dokumen");
+
+
+            $nama_dokumen = preg_replace("/\s+/", "_", $request->nama_dokumen);
+
+
+            $file_name = $nama_dokumen . "_" . $dtFormat;
+            // Membuat nama file baru dengan format tanggal dan waktu saat ini
+            $file_name = $nama_dokumen . "_" . $dtFormat . "." . $file->getClientOriginalExtension();
+
+            // Memeriksa apakah file yang diunggah adalah file dokumen yang valid (DOC atau DOCX)
+            if ($this->__cekFileDokumen($file)) {
+
+                $uploadPath = $file->storeAs("dokumen/masuk", $file_name . '.docx', "public");
+
+
+                $convert = new OfficeConverter(storage_path("app/public/" . $uploadPath));
+                $pdfFileName = pathinfo($convert->convertTo($file_name . '.pdf'), PATHINFO_FILENAME) . '.pdf';
+
+
+                Storage::disk("public")->delete($uploadPath);
+
+
+                $pdfFileCompress = new PdfOptimzer(storage_path("app/public/dokumen/masuk/" . $pdfFileName), storage_path("app/public/dokumen/masuk"));
+                $pdfCompressName = $pdfFileCompress->convertPdf();
+
+
+                Storage::disk("public")->delete("dokumen/masuk/" . $pdfFileName);
+
+
+                $data["lampiran"] = "dokumen/masuk/" . pathinfo($pdfCompressName, PATHINFO_FILENAME) . ".pdf";
+            } elseif ($file->getClientOriginalExtension() == "pdf") {
+
+                $uploadPath = $file->storeAs("dokumen/masuk", $file_name . '.pdf', "public");
+
+
+                $pdfFileCompress = new PdfOptimzer(storage_path("app/public/" . $uploadPath), storage_path("app/public/dokumen/masuk"));
+                $pdfCompressName = $pdfFileCompress->convertPdf();
+
+
+                Storage::disk("public")->delete($uploadPath);
+
+
+                $data["lampiran"] = "dokumen/masuk/" . pathinfo($pdfCompressName, PATHINFO_FILENAME) . ".pdf";
+            } else {
+
+                return redirect()->back()->with("error", "File yang diunggah harus berupa file dokumen (DOC, DOCX, atau PDF)!")->withInput();
+            }
+        }
 
         $arsip_masuk->update($data);
 
@@ -79,4 +163,17 @@ class ArsipMasukController extends Controller
     //     return response()->download(storage_path('app/public/' . $path_pdf));
     // }
 
+    /**
+     * Memeriksa apakah ekstensi file yang diunggah termasuk dalam daftar ekstensi yang diperbolehkan.
+     *
+     * @param \Illuminate\Http\UploadedFile $file
+     * @return bool
+     */
+    protected function __cekFileDokumen($file) {
+        $daftarExtensi = ["doc", "docx"];
+
+        $extensiDariFile = $file->getClientOriginalExtension();
+
+        return in_array($extensiDariFile, $daftarExtensi);
+    }
 }
--- resources/views/admin/arsip_masuk/edit_arsipMasuk.blade.php
+++ resources/views/admin/arsip_masuk/edit_arsipMasuk.blade.php
@@ -2,107 +2,137 @@
 @section('title', 'Edit Arsip Dokumen Masuk')
 @section('content')
 
-<div class="main-content side-content pt-0">
+    <div class="pt-0 main-content side-content">
 
-    <div class="main-container container-fluid">
-        <div class="inner-body">
-            <!-- Page Header -->
-            <div class="page-header text-center" style="margin-bottom: 20px;">
-                <div>
-                    <h2 class="main-content-label tx-24 mg-b-5" style="color: darkslateblue; font-weight: bold; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
-                        <i class="fas fa-edit" style="margin-right: 10px; font-size: 28px;"></i>
-                        Edit Dokumen
-                    </h2>
+        <div class="main-container container-fluid">
+            <div class="inner-body">
+                <!-- Page Header -->
+                <div class="text-center page-header" style="margin-bottom: 20px;">
+                    <div>
+                        <h2 class="main-content-label tx-24 mg-b-5"
+                            style="color: darkslateblue; font-weight: bold; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
+                            <i class="fas fa-edit" style="margin-right: 10px; font-size: 28px;"></i>
+                            Edit Dokumen
+                        </h2>
+                    </div>
                 </div>
-            </div>
-            <div class="card custom-card">
-                <div class="card-header">
-                    Dokumen Masuk
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-6">
-                            <form action="/admin/arsip_masuk/update/{{ $arsip_masuk->id }}" method="POST" enctype="multipart/form-data">
-                                {{-- enctype wajib seperti itu untuk mengupload file  --}}
-                            @csrf
-                            @method('PUT')
-                                {{-- @if ($errors->any())
-                                    <div class="alert alert-danger text-center">
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </div>
-                                @endif --}}
+                <div class="card custom-card">
+                    <div class="card-header">
+                        Dokumen Masuk
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <form action="/admin/arsip_masuk/update/{{ $arsip_masuk->id }}" method="POST"
+                                    enctype="multipart/form-data">
+                                    {{-- enctype wajib seperti itu untuk mengupload file  --}}
+                                    @csrf
+                                    @method('PUT')
 
-                                <div class="content">
-                                    <div class="row">
-                                        <div class="col-sm-15">
-                                            <div class="form-group">
-                                                <label class="tx-medium">Tanggal</label>
-                                                <input type="date" class="form-control" name="tanggal_keluar" id="tanggal_keluar"
-                                                    value="{{ old('tanggal_masuk')??$arsip_masuk->tanggal_masuk }}" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="namaDinas" class="tx-medium">Dinas</label>
-                                                <select id="namaDinas" name="dinas_id" class="form-control">
-                                                    <!-- Option list remains unchanged -->
-                                                    <option value="" selected>Pilih</option>
+                                    <div class="content">
+                                        <div class="row">
+                                            <div class="col-sm-15">
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Tanggal</label>
+                                                    <input type="date" class="form-control" name="tanggal_masuk"
+                                                        id="tanggal_masuk"
+                                                        value="{{ old('tanggal_masuk') ?? $arsip_masuk->tanggal_masuk }}"
+                                                        required>
+                                                    @error('tanggal_masuk')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="namaDinas" class="tx-medium">Dinas</label>
+                                                    <select id="namaDinas" name="dinas_id" class="form-control">
+                                                        <!-- Option list remains unchanged -->
+                                                        <option value="" selected>Pilih</option>
                                                         @foreach ($instansi as $data)
-                                                            <option value="{{$data->id}}" {{ old('instansi_id', $arsip_masuk->instansi_id) == $data->id ? 'selected' : '' }}>{{$data->nama_instansi}}</option>
+                                                            <option value="{{ $data->id }}"
+                                                                {{ old('instansi_id', $arsip_masuk->instansi_id) == $data->id ? 'selected' : '' }}>
+                                                                {{ $data->nama_instansi }}</option>
                                                         @endforeach
-                                                </select>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="tx-medium">Pengirim</label>
-                                                <input type="text" class="form-control" name="nama_pengirim" id="nama_pengirim" value="{{ old('pengirim')??$arsip_masuk->pengirim }}" >
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="tx-medium">Penerima</label>
-                                                <input type="text" class="form-control" name="nama_penerima" id="nama_penerima" value="{{ old('penerima')??$arsip_masuk->penerima }}">
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="tx-medium">Nama Dokumen</label>
-                                                <input type="text" class="form-control" name="nama_dokumen" id="nama_dokumen" value="{{ old('nama_dokumen')??$arsip_masuk->nama_dokumen }}" required>
-                                            </div>
-                                            <div class=" form-group">
-                                                <label>Kategori Dokumen:</label>
+                                                    </select>
+                                                    @error('dinas_id')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Pengirim</label>
+                                                    <input type="text" class="form-control" name="nama_pengirim"
+                                                        id="nama_pengirim"
+                                                        value="{{ old('pengirim') ?? $arsip_masuk->pengirim }}">
+                                                    @error('nama_pengirim')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Penerima</label>
+                                                    <input type="text" class="form-control" name="nama_penerima"
+                                                        id="nama_penerima"
+                                                        value="{{ old('penerima') ?? $arsip_masuk->penerima }}">
+                                                    @error('nama_penerima')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Nama Dokumen</label>
+                                                    <input type="text" class="form-control" name="nama_dokumen"
+                                                        id="nama_dokumen"
+                                                        value="{{ old('nama_dokumen') ?? $arsip_masuk->nama_dokumen }}"
+                                                        required>
+                                                    @error('nama_dokumen')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class=" form-group">
+                                                    <label>Kategori Dokumen:</label>
                                                     @foreach ($kategori as $data)
                                                         <div>
-                                                            <input type="radio" id="pilihan{{$data->id}}" name="kategori_id"
-                                                                value="{{$data->id}}" {{ (old('dokumen_kategori_id', $arsip_masuk->dokumen_kategori_id) == $data->id) ? 'checked' : '' }}>
-                                                            <label for="pilihan{{$data->id}}">{{$data->nama_kategori}}</label>
+                                                            <input type="radio" id="pilihan{{ $data->id }}"
+                                                                name="kategori_id" value="{{ $data->id }}"
+                                                                {{ old('dokumen_kategori_id', $arsip_masuk->dokumen_kategori_id) == $data->id ? 'checked' : '' }}>
+                                                            <label
+                                                                for="pilihan{{ $data->id }}">{{ $data->nama_kategori }}</label>
                                                         </div>
                                                     @endforeach
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="tx-medium">Lampiran Dokumen</label>
-                                                <input type="file" name="file_dokumen" id="file_dokumen" class="form-control">
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="tx-medium">Keterangan</label>
-                                                <textarea name="keterangan" rows="3"
-                                                    class="form-control">{{ old('keterangan') }}</textarea>
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                                    @error('kategori_id')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Lampiran Dokumen</label>
+                                                    <input type="file" name="file_dokumen" id="file_dokumen"
+                                                        class="form-control">
+                                                    @error('file_dokumen')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="tx-medium">Keterangan</label>
+                                                    <textarea name="keterangan" rows="3" class="form-control">{{ old('keterangan') }}</textarea>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-6" style="margin-top: 30px">
-                            <div class="pdf-viewer-container">
-                                <iframe id="pdf-viewer" src="{{ asset('/laraview/#../storage/'.$arsip_masuk->lampiran) }}" width="100%" height="700px" style="border: none;"
-                                    allowfullscreen webkitallowfullscreen></iframe>
+                                </form>
                             </div>
-                        </div>
+                            <div class="col-6" style="margin-top: 30px">
+                                <div class="pdf-viewer-container">
+                                    <iframe id="pdf-viewer"
+                                        src="{{ asset('/laraview/#../storage/' . $arsip_masuk->lampiran) }}" width="100%"
+                                        height="700px" style="border: none;" allowfullscreen webkitallowfullscreen></iframe>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 @endsection
