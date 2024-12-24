5c9c50da64ebee8cfcd1ced3bea56d91cb911ba6
ryzntx <thalalatha13@gmail.com>
2024-12-24 08:49:31 +0700

    fix: streamline form layout and improve error message display in add instansi view

--- resources/views/admin/instansi/add_instansi.blade.php
+++ resources/views/admin/instansi/add_instansi.blade.php
@@ -19,77 +19,60 @@
                 <div class="card-header">
                     Tambah Data
                 </div>
-                    <div class="card-body">
-                        <form action="/admin/kelola_instansi/insert" method="POST" enctype="multipart/form-data">
-                    {{-- enctype wajib seperti itu untuk mengupload file  --}}
+                <div class="card-body">
+                    <form action="/admin/kelola_instansi/insert" method="POST" enctype="multipart/form-data">
+                        {{-- enctype wajib seperti itu untuk mengupload file  --}}
                         @csrf
 
-                        @if ($errors->any())
-                            <div class="alert alert-danger text-center">
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                            </div>
-                            @endif
-
-
-
-                            <div class="content">
-                                <div class="row">
-                                    <div class="col-sm-15">
-                                        <div class="form-group">
-                                            <label>Nama Instansi</label>
-                                            <input type="text" name="nama_instansi" class="form-control" value="{{ old('nama_instansi')}}">
-                                            <div class="text-danger">
-                                                @error('nama_instansi')
-                                                {{ $message }}
-                                                @enderror
-                                            </div>
-                                            {{-- untuk menampilkan pesan error --}}
+                        <div class="content">
+                            <div class="row">
+                                <div class="col-sm-15">
+                                    <div class="form-group">
+                                        <label>Nama Instansi</label>
+                                        <input type="text" name="nama_instansi" class="form-control"
+                                            value="{{ old('nama_instansi')}}">
+                                        <div class="text-danger">
+                                            @error('nama_instansi')
+                                            {{ $message }}
+                                            @enderror
                                         </div>
-
-
-                                        <div class="form-group">
-                                            <label>Inisial Instansi</label>
-                                            <input type="text" name="singkatan_instansi" class="form-control" value="{{ old('singkatan_instansi')}}">
-                                            <div class="text-danger">
-                                                @error('singkatan_instansi')
-                                                {{ $message }}
-                                                @enderror
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Alamat Instansi</label>
-                                            <input type="text" name="alamat" class="form-control" value="{{ old('alamat')}}">
-                                            <div class="text-danger">
-                                                @error('alamat')
-                                                {{ $message }}
-                                                @enderror
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-primary">Simpan</button>
-                                        </div>
-
+                                        {{-- untuk menampilkan pesan error --}}
                                     </div>
+
+
+                                    <div class="form-group">
+                                        <label>Inisial Instansi</label>
+                                        <input type="text" name="singkatan_instansi" class="form-control"
+                                            value="{{ old('singkatan_instansi')}}">
+                                        <div class="text-danger">
+                                            @error('singkatan_instansi')
+                                            {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Alamat Instansi</label>
+                                        <input type="text" name="alamat" class="form-control"
+                                            value="{{ old('alamat')}}">
+                                        <div class="text-danger">
+                                            @error('alamat')
+                                            {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+
                                 </div>
                             </div>
+                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-{{-- 1.Gunakan content jika itu adalah kelas yang Anda buat sendiri atau berasal dari kerangka kerja khusus.
-2.Gunakan row untuk mengelompokkan elemen dalam tata letak berbasis grid.
-3.Gunakan col-sm-6 (atau variasi lainnya) untuk mengatur lebar kolom. --}}
-
-
-
-
-
-
-
 
 @endsection
